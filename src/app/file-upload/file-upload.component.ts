f251299dd37ffe6b29936cf390b999c3a231a584
Sagar <sagar.j.rao98@gmail.com>
2025-04-24 23:29:45 -0400

    added the upload service and connected to backend

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,26 +1,54 @@
 import { Component } from '@angular/core';
-
+import { UploadService } from '../services/upload.service';
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent {
-
   selectedFile: File | null = null;
   selectedFileName = '';
+  isUploading = false;
+  constructor(private uploadService: UploadService) {}
 
   onFileSelected(event: Event) {
-      const input = event.target as HTMLInputElement;
-
-      if (input.files && input.files.length > 0) {
-          const file = input.files[0];
-          this.selectedFile = file;
-
-          this.selectedFileName = file.name;
-      }
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      this.selectedFileName = this.selectedFile.name;
+    }
   }
+
   onUpload() {
-      console.log("Uploading...", this.selectedFile);
+    if (!this.selectedFile) return;
+  
+    this.isUploading = true;
+  
+    const file = this.selectedFile;
+    const filename = file.name;
+    const filetype = file.type;
+  
+    this.uploadService.getSignedUploadUrl(filename, filetype).subscribe({
+      next: ({ url }) => {
+        this.uploadService.uploadFileToS3(url, file).subscribe({
+          next: (event) => {
+            console.log('Upload event:', event);
+          },
+          complete: () => {
+            console.log('Upload finished');
+            this.isUploading = false;
+          },
+          error: (err) => {
+            console.error('Upload failed', err);
+            this.isUploading = false;
+          }
+        });
+      },
+      error: (err) => {
+        console.error('Error generating URL', err);
+        this.isUploading = false;
+      }
+    });
   }
+  
 }
---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  private backendUrl = 'http://localhost:3000';
+  constructor(private http: HttpClient) {}
+
+  getSignedUploadUrl(filename: string, filetype: string) {
+    return this.http.post<{ url: string }>(
+      `${this.backendUrl}/generate-upload-url`,
+      { filename, filetype }
+    );
+  }
+
+  uploadFileToS3(signedUrl: string, file: File) {
+    return this.http.put(signedUrl, file, {
+      headers: {
+        'Content-Type': file.type
+      },
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+}
